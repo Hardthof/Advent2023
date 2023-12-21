09b8b1d0788baf53fd393345f7e422ac00d572b7
Hardthof <Philippe.Cheray@gmail.com>
2023-12-21 20:28:36 +0100

    day3

--- Day3.py
+++ Day3.py
@@ -1,42 +1,46 @@
-import re
+import numpy as np 
 
-bag = {'red':12, 'green':13, 'blue':14}
+def hasAdjacentSymbol(symbols, candidate, xlim, ylim):
+    # print(symbols)
+    print(candidate)
 
-def search(s):
-    for item in s:
-            for key in bag.keys():
-                if key in item:
-                    if(int(re.findall(r'\d+', item)[0]) > bag[key]):
-                         return False
-    return True
+    n = set()
+    for i in range(len(candidate)):
+        if i == 0:
+            # right
+            if candidate[0] +1 < xlim: n.add((candidate[0] + 1, candidate[1]))
+        if i == len(candidate) -1:
+            if candidate[0] > 0: n.add((candidate[0] - 1, candidate[1]))
+        
+        if candidate[1] +1 < ylim: n.add((candidate[0], candidate[1] + 1))
+        if candidate[1] > 0: n.add((candidate[0], candidate[1] - 1))
 
-def calcpower(s):
-    red = 0
-    blue = 0
-    green = 0
+    return any(entry in symbols for entry in candidate)
 
-    for item in s:
-        if 'red' in item: red = max(red, int(re.findall(r'\d+', item)[0]))
-        elif 'blue' in item: blue = max(blue, int(re.findall(r'\d+', item)[0]))
-        elif 'green' in item: green = max(green, int(re.findall(r'\d+', item)[0]))
-
-    return red* blue* green
 
 if __name__ == '__main__':
 
-    f = open('Day2.txt', mode='r')
-    seq = [line.split(':')[1].strip() for line in f]
-    
-    result = 0
-    setpower = 0
-    for i,s in enumerate(seq):
-        ss = s.split(';')
-        sss = []
-        for k in ([f.split(',') for f in ss]):
-            for j in k:
-                sss.append(j)
-        
-        if search(sss): result = result + i + 1
-        setpower += calcpower(sss)
+    matrix = []
+    with open('Day3.txt', 'r') as file:
+        for line in file:
+            row = list(line.strip())
+            matrix.append(row) 
 
-    print(result, setpower)
+    symbols = set()
+    numberCandidates = set()
+
+    for x,line in enumerate(matrix):
+        number = ''
+        for y,c in enumerate(line):
+            if c.isdigit(): 
+                number += c    
+            elif c == '.': 
+                if number:
+                    numberCandidates.add((x,y, number))
+                    number = ''
+            else:
+                symbols.add((x,y))
+    
+    for candidate in numberCandidates:
+        print(hasAdjacentSymbol(symbols, candidate, len(matrix), len(matrix[0])))
+    
